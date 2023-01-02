18d26cdb307262070fdb29632f835feaf2f40280
Javier Castaneda <castaneda.javier.j@gmail.com>
2023-01-02 12:25:52 -0600

    working on exercise for Java intro

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,38 @@
 public class HelloWorld {
     public static void main (String[] args) {
+
         System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 87;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "i like to skate";
+        System.out.println(myString);
+
+//        double myNumber = 123L;
+//        System.out.println(myNumber);
+
+//        double myNumber = 3.14;
+//        System.out.println(myNumber);
+
+//        using double to work the decimal number
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+
+//        double myNumber = 3.14F;
+//        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);     //adds after so we dont see result.
+//        System.out.println(x);
+
+//        int y = 5;
+//        System.out.println(++x);    //adds before so we see the addition.
+//        System.out.println(x);
+
+//    String class = "Hello World";
+
+
+
     }
 }
