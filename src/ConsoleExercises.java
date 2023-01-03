1ba9afdc36af6ccb07d6b9e6d08d3d46fd9e945b
Javier Castaneda <castaneda.javier.j@gmail.com>
2023-01-03 11:08:19 -0600

    completed java exercise

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+
+    public static void main (String[] args){
+        Scanner scanny = new Scanner(System.in);
+
+
+        double pi = 3.14159;
+        System.out.printf("The value of pi is approximately %.2f\n", pi);
+//    System.out.format("The value of pi is approximately %.2f", pi);
+
+        System.out.println("Please pick a number.");
+        int randoNumber = scanny.nextInt();
+        System.out.println(("Your number is "+randoNumber));
+
+        System.out.println("Please enter 3 words, separated by a space");
+        String wordOne = scanny.next();
+        String wordTwo = scanny.next();
+        String wordThree = scanny.next();
+//        System.out.println("This is your words "+wordOne +" " +wordTwo +" " +wordThree);
+        System.out.printf("The first word was %s.%n The second word is %s.%n The last word is %s %n", wordOne, wordTwo, wordThree);
+
+        scanny.nextLine();
+        System.out.println("Type in a sentence please");
+        String sentenceOne = scanny.nextLine();
+        System.out.printf("Your sentence is: %s %n", sentenceOne );
+
+
+        System.out.println("What is the width?");
+        int width = Integer.parseInt(scanny.nextLine());
+        System.out.println("What is the length?");
+        int length = Integer.parseInt(scanny.nextLine());
+
+        float perimeter = (width * 2) + (length * 2);
+        System.out.println("The perimeter is "+perimeter);
+
+
+
+
+
+
+    }
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,25 +2,31 @@ public class HelloWorld {
     public static void main (String[] args) {
 
         System.out.println("Hello, World!");
-
+//        exercise 1
         int myFavoriteNumber = 87;
         System.out.println(myFavoriteNumber);
-
+//        exercise 2 , 3, & 4
         String myString = "i like to skate";
         System.out.println(myString);
+//        exercise 5
+        long myNumber = 9999;
+        System.out.println(myNumber);
+//        exercise 7
+        long otherNumber = 123L;
+        System.out.println(otherNumber);
+//        exercise 6
+        double myDouble = 3.14;
+        System.out.println(myDouble);
 
-//        double myNumber = 123L;
-//        System.out.println(myNumber);
-
+//        using double to work the decimal number
 //        double myNumber = 3.14;
 //        System.out.println(myNumber);
 
-//        using double to work the decimal number
-        double myNumber = 3.14;
-        System.out.println(myNumber);
-
 //        double myNumber = 3.14F;
 //        System.out.println(myNumber);
+
+//        float myNumber = 3.14F;
+//        float myNum = (float) 3.14;
 
 //        int x = 5;
 //        System.out.println(x++);     //adds after so we dont see result.
@@ -30,9 +36,31 @@ public class HelloWorld {
 //        System.out.println(++x);    //adds before so we see the addition.
 //        System.out.println(x);
 
-//    String class = "Hello World";
+//    String class = "class";
 
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) "three";
 
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+//
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//        System.out.println(y);
+//
+//
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE - 1);
 
     }
 }
+
