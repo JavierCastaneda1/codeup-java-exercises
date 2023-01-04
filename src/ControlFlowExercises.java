50ce4e2d023e3cf8e963ed494e91d29653e2e680
Javier Castaneda <castaneda.javier.j@gmail.com>
2023-01-03 19:23:06 -0600

    working on next exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,10 +3,24 @@ import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main (String[] args){
 
-        int i = 5;
-        while (i <= 15) {
-            System.out.println(i++);
-        }
+//    EXERCISE 1
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println(i++);
+//        }
+
+//        EXERCISE 2
+//        int number = 0;
+//        do {
+//            System.out.println(number);
+//            number += 2;
+//        } while (number <= 100);
+
+        //        EXERCISE 3
+//        int number = 105;
+//        do {
+//            System.out.println(number -= 5);
+//        } while (number > -10);
 
 
 
