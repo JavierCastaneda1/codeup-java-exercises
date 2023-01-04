318a84bd212ce97774a1b4c8fdc1d7fdef4795aa
Javier Castaneda <castaneda.javier.j@gmail.com>
2023-01-04 16:14:27 -0600

    exercise complete for strings

---
+++ src/Bob.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main (String[] args) {
+
+        Scanner scanny = new Scanner(System.in);
+
+        System.out.println("What would you like to tell Bob?");
+        String input = scanny.nextLine();
+
+        if (input.endsWith("?")){
+            System.out.println("Bob says: Sure.");
+        } else if (input.endsWith("!")){
+            System.out.println("Bob says: Whoa, chill out!");
+        } else if (input.isEmpty()){
+            System.out.println("Bob Says: Fine. Be that way!");
+        } else {
+            System.out.println("Bob says: Whatever");
+        }
+
+    }
+}
