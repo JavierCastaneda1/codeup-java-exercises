7c76c62d3e23b3d82862d7d88e868d6e3ccecba4
Javier Castaneda <castaneda.javier.j@gmail.com>
2023-01-04 11:19:53 -0600

    working on final exercise for java loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,26 +1,91 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class ControlFlowExercises {
     public static void main (String[] args){
+        Scanner scanny = new Scanner(System.in);
 
-//    EXERCISE 1
+//    EXERCISE 1 (WHILE LOOP)
 //        int i = 5;
 //        while (i <= 15) {
 //            System.out.println(i++);
 //        }
 
-//        EXERCISE 2
+//        EXERCISE 2 (DO WHILE LOOP)
 //        int number = 0;
 //        do {
 //            System.out.println(number);
 //            number += 2;
 //        } while (number <= 100);
 
-        //        EXERCISE 3
+        //        EXERCISE 3 (DO WHILE REVERSE)
 //        int number = 105;
 //        do {
 //            System.out.println(number -= 5);
 //        } while (number > -10);
+
+//        EXERCISE 4 (DO WHILE LOOP)
+
+//        long num = 2;
+//        do {
+//            System.out.println(num);
+//            num *= num;
+//        } while (num < 1000000);
+
+//        EXERCISE 5 (FOR LOOP VERSIONS)
+//        for(int i = 5; i <= 15; i++) {
+//            System.out.println(i);
+//        }
+
+//        FOR LOOP VERSION
+//        for(int i = 2; i <= 100; i+=2) {
+//            System.out.println(i);
+//        }
+
+//        for(int i = 100; i >= -10; i -= 5) {
+//            System.out.println(i);
+//        }
+
+//        for (long i = 2; i < 1000000; i *= i) {
+//            System.out.println(i);
+//        }
+
+//        FIZZ BUZZ
+
+//        for (int i = 1; i <= 100; i++) {
+//            if (i % 3 == 0 && i % 5 == 0){
+//                System.out.println("FizzBuzz");
+//            } else if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            }
+//            else {
+//                System.out.println(i);
+//            }
+//        }
+
+//        TABLE EXERCISE
+//            String confirm;
+//            do {
+//                System.out.println("Enter the ending number for your table: ");
+//                int input = scanny.nextInt();
+//                System.out.println("Number\tSquared\tCubed");
+//                System.out.println("------\t-------\t-----");
+//                for (int i = 1; i <= input; i++) {
+//                    System.out.println(i + "\t\t" + i * i + "\t\t" + i * i * i);
+//                }
+//                System.out.println("Would you like to try again? yes or no?");
+//                confirm = scanny.next();
+//            }
+//            while (confirm.equals("yes"));
+
+
+
+
+
+
+
 
 
 
