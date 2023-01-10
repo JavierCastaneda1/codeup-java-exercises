5d7dbf9cfc739736e00103e013572c254b7f9a2a
Javier Castaneda <castaneda.javier.j@gmail.com>
2023-01-09 20:14:23 -0600

    practice exercise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -4,7 +4,7 @@ public class ArraysExercises {
     public static void main(String[] args){
 
         int[] numbers = {1, 2, 3, 4, 5};
-        System.out.println(numbers); //this one spits out memory location thing
+        System.out.println(numbers[2]); //this one spits out the "i" indexed out.
         System.out.println(Arrays.toString(numbers));
 
         Person[] people = new Person[3];
@@ -15,6 +15,10 @@ public class ArraysExercises {
         for (Person person : people) {
             System.out.println(person.getName());
         }
+
+//        for (Person person : people) {
+//            System.out.println(person.getName());
+//        }
 
 
 
