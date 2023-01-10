1f3fb164a5136f9fd4866f086d32f54c51663824
Javier Castaneda <castaneda.javier.j@gmail.com>
2023-01-10 12:05:07 -0600

    working on previous exercise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,6 +1,13 @@
 import java.util.Arrays;
 
 public class ArraysExercises {
+    public static Person[] addPerson(Person[] array, Person newPerson){
+        Person[] people = Arrays.copyOf(array, array.length +1 );
+        people[array.length] = newPerson;
+        return people;
+    }
+
+
     public static void main(String[] args){
 
         int[] numbers = {1, 2, 3, 4, 5};
@@ -15,18 +22,12 @@ public class ArraysExercises {
         for (Person person : people) {
             System.out.println(person.getName());
         }
-
-//        for (Person person : people) {
-//            System.out.println(person.getName());
-//        }
-
-
-
-
-
-
-
-
+        System.out.println("-----------------");
+        Person sophia = new Person("Sophia");
+        people = addPerson(people, sophia);
+        for (Person person : people) {
+            System.out.println(person.getName());
+        }
     }
 
 }
