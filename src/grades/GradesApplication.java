0c573b4192f94bd755b92545bf7bbafeb00c56d6
Javier Castaneda <castaneda.javier.j@gmail.com>
2023-01-13 10:29:20 -0600

    working on grades application exercise

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -42,6 +42,48 @@ public class GradesApplication {
 
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Welcome to my application!");
+        String tryAgain = "";
+
+
+        do{
+            System.out.println("Here are the github names of students: ");
+            System.out.println(gitStudentName.keySet());
+            System.out.println("What student would you like to see more information on?");
+            String userPick = scanner.next();
+
+            if (userPick.equalsIgnoreCase("EdwardScissorHands111")) {
+
+                System.out.println("Name: "+ gitStudentName.get("EdwardScissorHands111").getName() + "--" + "Github Username: EdwardScissorHands111");
+                System.out.println("Current average grade: "+ gitStudentName.get("EdwardScissorHands111").getGradeAverage());
+
+            } else if (userPick.equalsIgnoreCase("Achilles987")) {
+
+                System.out.println("Name: " + gitStudentName.get("Achilles987").getName() + "--" + "Github Username: Achilles987");
+                System.out.println("Current average grade: "+ gitStudentName.get("Achilles987").getGradeAverage());
+
+            } else if (userPick.equalsIgnoreCase("Moonwalker78")) {
+                System.out.println("Name: "+ gitStudentName.get("Moonwalker78").getName() + "--" + "Github Username: Moonwalker78");
+                System.out.println("Current average grade: "+ gitStudentName.get("Moonwalker78").getGradeAverage());
+
+            } else if (userPick.equalsIgnoreCase("AirJordan48")) {
+                System.out.println("Name: "+ gitStudentName.get("AirJordan48").getName() + "--" + "Github Username: AirJordan48");
+                System.out.println("Current average grade: "+ gitStudentName.get("AirJordan48").getGradeAverage());
+
+            } else if (userPick.equalsIgnoreCase("Birdman54")) {
+                System.out.println("Name: "+ gitStudentName.get("Birdman54").getName() + "--" + "Github Username: Birdman54");
+                System.out.println("Current average grade: "+ gitStudentName.get("Birdman54").getGradeAverage());
+            } else {
+                scanner.next();
+                System.out.println("Error, there is no user with this name");
+            }
+            System.out.println("Do you want to try again? (y/n)");
+            tryAgain = scanner.next();
+
+
+        } while (tryAgain.equalsIgnoreCase("y"));
+        System.out.println("Laterz");
+
 
     }
 }
