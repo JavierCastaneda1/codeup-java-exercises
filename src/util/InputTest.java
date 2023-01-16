c454b238b8009ce405e2a324ed5d13e8cafa44ec
Javier Castaneda <castaneda.javier.j@gmail.com>
2023-01-15 22:15:38 -0600

    java

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -4,7 +4,9 @@ public class InputTest {
     public static void main(String[] args){
 
         Input input = new Input();
+
         String userString = input.getString();
+
         System.out.printf("User input: %s%n", userString);
         System.out.println(input.yesNo());
         System.out.println(input.getInt(1,100));
